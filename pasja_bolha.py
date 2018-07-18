b23bc240db3096e285c4f851032e29530a6bf777
MocnikS <mocnik.sara@gmail.com>
2018-07-18 08:22:28 +0200

    Add files via upload
    Popravljena verzija

--- pasja_bolha.py
+++ pasja_bolha.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import psycopg2
 import sys
+import re
 
 from bottle import *
 import auth_public as auth
@@ -21,12 +22,27 @@ conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, passwo
 conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogocimo transakcije
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-# conn.cursor will return a cursor object, you can use this cursor to perform queries
-
 print ("Connected!\n")
 
 
+######################################################################
+# Pomozne funkcije:
 
+def stevilka(besedilo):
+    '''Spremeni besedilo s številko v številko.
+    Uporabimo, da izbran radio button spremenimo v njegovo vrednost.
+    Če ni bil izbran noben, izpiše 0.'''
+    stevilka = 0
+
+    if besedilo == None:
+        return stevilka
+    
+    for znak in besedilo:
+        if znak in "12345":
+            stevilka = int(znak)
+            return stevilka
+
+            
 ######################################################################
 # Funkcije za izgradnjo strani
 
@@ -40,37 +56,100 @@ def static(filename):
 # Glavna stran
 @route('/')
 def zacetna_stran():
-# Tu bi morali dobiti podatke iz baze
     return template('zacetna_stran')
 
 # Stran za iskanje idealne pasme
 @route('/izbira_psa/')
 def izbira_psa():
+    notranjost = request.query.get('notranjost')
+    izkusnje = request.query.get('izkusnje')
+    obcutljivost = request.query.get('obcutljivost')
+    samota = request.query.get('samota')
+    hladno = request.query.get('hladno')
+    toplo = request.query.get('toplo')
+    druzina = request.query.get('druzina')
+    otroci = request.query.get('otroci')
+    drugi_psi = request.query.get('drugi_psi')
+    tujci = request.query.get('tujci')
+    dlaka = request.query.get('dlaka')
+    slinjenje = request.query.get('slinjenje')
+    dlaka_skrb = request.query.get('dlaka_skrb')
+    zdravje = request.query.get('zdravje')
+    debelost = request.query.get('debelost')
+    velikost = request.query.get('velikost')
+    ucljivost = request.query.get('ucljivost')
+    inteligenca = request.query.get('inteligenca')
+    grizenje = request.query.get('grizenje')
+    lovski_nagon = request.query.get('lovski_nagon')
+    lajanje = request.query.get('lajanje')
+    potepanje = request.query.get('potepanje')
+    energicnost = request.query.get('energicnost')
+    utrujenost = request.query.get('utrujenost')
+    gibanje = request.query.get('gibanje')
+    igrivost =  request.query.get('igrivost')
+    izbrano = [stevilka(notranjost), stevilka(izkusnje), stevilka(obcutljivost),
+               stevilka(samota), stevilka(hladno), stevilka(toplo), stevilka(druzina),
+               stevilka(otroci), stevilka(drugi_psi), stevilka(tujci), stevilka(dlaka),
+               stevilka(slinjenje), 6-stevilka(dlaka_skrb), stevilka(zdravje),
+               stevilka(debelost), stevilka(velikost), stevilka(ucljivost),
+               stevilka(inteligenca), stevilka(grizenje), stevilka(lovski_nagon),
+               stevilka(lajanje), stevilka(potepanje), stevilka(energicnost),
+               stevilka(utrujenost), stevilka(gibanje), stevilka(igrivost)]
+    print(izbrano)
+
     return template('izbira_psa')
 
 # Stran za prijavo in registracijo
-@route('/prijava/')
+@route('/prijava/', method='GET')
 def prijava():
+    username = request.query.get('username')
+    password = request.query.get('password')
+
+    print(username, password)
+
+    cur.execute('''SELECT ime, geslo FROM uporabnik''')
+    rows = cur.fetchall()
+    for row in rows:
+        ime, geslo = row
+
+        if ime == username and geslo == password:
+            print("Najdeno")
+            break
+
+        print("Ni v bazi")
+        
     return template('prijava')
+
 
 @route('/registracija/')
 def registracija():
+    
     cur.execute('''SELECT posta FROM posta ''')
     rows = cur.fetchall() # prebere zgornji select in ga zapoše v rows v obliki (postna_st, posta, regija)
     postne_st = []
     poste = []
     regije = []
+    stevilka_posta = []
     for row in rows:
         (postna_st, posta, regija) = tuple(row[0].split(','))
+        posta = re.sub('"', '', posta)
         postna_st = int(postna_st[1:])
         regija = int(regija[:-1])
         postne_st.append(postna_st)
         poste.append(posta)
         regije.append(regija)
-    return template('registracija1.html',
+        stevilka_posta.append(str(postna_st) + " " + str(posta.strip()))
+    stevilka_posta.sort()
+
+    geslo1 = request.query.get('inputPassword1')
+    geslo2 = request.query.get('inputPassword2')
+    print(geslo1, geslo2)
+
+    return template('registracija.tpl',
                     postne_st=postne_st,
                     regije=regije,
-                    poste=poste)
+                    poste=poste,
+                    stevilka_posta=stevilka_posta)
 
 @route('/oglasi/')
 def oglasi():
@@ -78,7 +157,16 @@ def oglasi():
 
 @route('/ustvari_oglas/')
 def ustvari_oglas():
-    return template('ustvari_oglas')
+
+    cur.execute('''SELECT slovensko_ime FROM pasma ''')
+    rows = cur.fetchall() # prebere zgornji select in ga zapoše v rows v obliki (postna_st, posta, regija)
+    pasme = []
+    for row in rows:
+        row = str(row).strip('[]""''')
+        pasme.append(row)
+        
+    return template('ustvari_oglas.tpl',
+                    pasme=pasme)
 
 
 # Glavni program
